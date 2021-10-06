5e6ea2f9779c834cf30c6b9ec1d1a71d11c905ce
Donatron <don.macarthur68@gmail.com>
2021-10-06 10:55:08 +0700

    redux: added store and base reducers

---
+++ src/store/reducers/cocktailReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  isRandom: false,
+  cocktails: {}
+}
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
