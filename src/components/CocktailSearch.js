f5aaf3d729cb9e89d54f2f4654fe47929b359b00
Donatron <don.macarthur68@gmail.com>
2021-10-13 14:13:50 +0700

    search-filter: component level state to cocktailsearch

--- src/components/CocktailSearch.js
+++ src/components/CocktailSearch.js
@@ -5,16 +5,47 @@ const CocktailSearch = () => {
   const [searchByDropdownOpen, setSearchByDropdownOpen] = useState(false);
   const [alcoholContentDropdownOpen, setAlcoholContentDropdownOpen] = useState(false);
   const [glassTypeDropdownOpen, setGlassTypeDropdownOpen] = useState(false);
+  const [searchType, setSearchType] = useState(null);
+  const [searchTerm, setSearchTerm] = useState(null);
+  const [alcoholContent, setAlcoholContent] = useState(null);
+  const [glassType, setGlassType] = useState(null);
 
-  const toggleSearchDropdown = (e) => {
-    console.log(e.target);
-    setSearchByDropdownOpen(prevState => !prevState);
-  }
+  const toggleSearchDropdown = (e) => setSearchByDropdownOpen(prevState => !prevState);
   const toggleAlcoholContentDropdown = () => setAlcoholContentDropdownOpen(prevState => !prevState);
   const toggleGlassTypeDropdown = () => setGlassTypeDropdownOpen(prevState => !prevState);
+  const onSelectSearchType = (e) => {
+    clearSearchFilters();
+    setSearchType(e.target.value)
+  }
+  const onSearchTermChange = (e) => {
+    clearSearchFilters();
+    setSearchTerm(e.target.value)
+  };
+  const onSelectAlcoholContent = (e) => {
+    clearSearchFilters();
+    setAlcoholContent(e.target.value)
+  };
+  const onSelectGlassType = (e) => {
+    clearSearchFilters();
+    setGlassType(e.target.value)
+  };
 
-  const onChange = (e) => {
-    console.log(e.target.value);
+  const handleSearchSubmit = () => {
+    // Fire off action to search recipes
+    // Redirect to 
+  };
+
+  const showSubmitButton = () => {
+    if ((searchType === "name" || searchType === "ingredient") && (searchTerm !== null && searchTerm !== "")) return true;
+    if (searchType === "alcoholContent" && alcoholContent !== null) return true;
+    if (searchType === "glassType" && glassType !== null) return true;
+    if (searchType === "random") return true;
+  }
+
+  const clearSearchFilters = () => {
+    setSearchTerm(null);
+    setAlcoholContent(null);
+    setGlassType(null);
   }
 
   return (
@@ -25,45 +56,62 @@ const CocktailSearch = () => {
           Search By
         </DropdownToggle>
         <DropdownMenu>
-          <DropdownItem>Name</DropdownItem>
-          <DropdownItem>Ingredient</DropdownItem>
-          <DropdownItem>Alcohol Content</DropdownItem>
-          <DropdownItem>Glass Type</DropdownItem>
-          <DropdownItem>Random</DropdownItem>
+          <DropdownItem onClick={onSelectSearchType} value="name">Name</DropdownItem>
+          <DropdownItem onClick={onSelectSearchType} value="ingredient">Ingredient</DropdownItem>
+          <DropdownItem onClick={onSelectSearchType} value="alcoholContent">Alcohol Content</DropdownItem>
+          <DropdownItem onClick={onSelectSearchType} value="glassType">Glass Type</DropdownItem>
+          <DropdownItem onClick={onSelectSearchType} value="random">Random</DropdownItem>
         </DropdownMenu>
       </Dropdown>
-      <Dropdown isOpen={alcoholContentDropdownOpen} toggle={toggleAlcoholContentDropdown}>
-        <DropdownToggle caret>
-          Alcohol Content
-        </DropdownToggle>
-        <DropdownMenu>
-          <DropdownItem>Alcoholic</DropdownItem>
-          <DropdownItem>Non Alcoholic</DropdownItem>
-          <DropdownItem>Optional Alcohol</DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-      <Dropdown isOpen={glassTypeDropdownOpen} toggle={toggleGlassTypeDropdown}>
-        <DropdownToggle caret>
-          Glass Type
-        </DropdownToggle>
-        <DropdownMenu>
-          <DropdownItem>Highball Glass</DropdownItem>
-          <DropdownItem>Cocktail Glass</DropdownItem>
-          <DropdownItem>Old-fashioned Glass</DropdownItem>
-          <DropdownItem>Whiskey Glass</DropdownItem>
-          <DropdownItem>Collins Glass</DropdownItem>
-          <DropdownItem>Pousse Cafe Glass</DropdownItem>
-          <DropdownItem>Champagne Flute</DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-      <InputGroup>
-        <Input
-          placeholder="enter search term"
-          className="cocktail-search_input"
-          onChange={onChange}
-        />
-      </InputGroup>
-      <Button color="primary">Search</Button>
+      {
+        searchType === "alcoholContent" ? (
+          <Dropdown isOpen={alcoholContentDropdownOpen} toggle={toggleAlcoholContentDropdown}>
+            <DropdownToggle caret>
+              Alcohol Content
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem onClick={onSelectAlcoholContent} value="alcoholic">Alcoholic</DropdownItem>
+              <DropdownItem onClick={onSelectAlcoholContent} value="non alcoholic">Non Alcoholic</DropdownItem>
+              <DropdownItem onClick={onSelectAlcoholContent} value="optional alcohol">Optional Alcohol</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+
+        ) : null
+      }
+      {
+        searchType === "glassType" ? (
+          <Dropdown isOpen={glassTypeDropdownOpen} toggle={toggleGlassTypeDropdown}>
+            <DropdownToggle caret>
+              Glass Type
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem onClick={onSelectGlassType} value="highball glass">Highball Glass</DropdownItem>
+              <DropdownItem onClick={onSelectGlassType} value="cocktail glass">Cocktail Glass</DropdownItem>
+              <DropdownItem onClick={onSelectGlassType} value="old-fashioned glass">Old-fashioned Glass</DropdownItem>
+              <DropdownItem onClick={onSelectGlassType} value="whiskey glass">Whiskey Glass</DropdownItem>
+              <DropdownItem onClick={onSelectGlassType} value="collins glass">Collins Glass</DropdownItem>
+              <DropdownItem onClick={onSelectGlassType} value="pousse cafe glass">Pousse Cafe Glass</DropdownItem>
+              <DropdownItem onClick={onSelectGlassType} value="champagne flute">Champagne Flute</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+
+        ) : null
+      }
+      {
+        searchType === "name" || searchType === "ingredient" ? (
+          <InputGroup>
+            <Input
+              placeholder="enter search term"
+              className="cocktail-search_input"
+              onChange={onSearchTermChange}
+            />
+          </InputGroup>
+        ) : null
+      }
+      {
+        showSubmitButton() ? <Button color="primary" onClick={handleSearchSubmit}>Search</Button> : null
+      }
+
     </div>
   );
 }
