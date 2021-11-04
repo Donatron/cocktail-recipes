b9becd622c4312a8f1517f5f8f14d2f66f03ef80
Donatron <don.macarthur68@gmail.com>
2021-11-04 14:40:37 +0700

    Updates

--- src/components/ErrorMessage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Alert } from 'reactstrap';
-
-const ErrorMessage = ({ message }) => {
-  return <Alert color="danger">{message}</Alert>
-}
-
-export default ErrorMessage;
