5732bbbae96606e44f4316a82ce423f78f8f36d2
Donatron <don.macarthur68@gmail.com>
2021-11-10 04:40:26 +0700

    react-router: moved browser router to outermost wrapper of app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
@@ -7,10 +8,12 @@ import App from './App';
 import { store, persistor } from './store/store';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
